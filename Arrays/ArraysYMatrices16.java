41e9979ada4784f79b74f200a3f081fe8750f273
AdrianPerez2207 <aperezsanchez441@gmail.com>
2023-11-23 21:40:34 +0100

    clase commit

--- Arrays/ArraysYMatrices16.java
+++ Arrays/ArraysYMatrices16.java
@@ -6,16 +6,39 @@ public class ArraysYMatrices16 {
         int numeros = (int) (Math.random() * 50) + 1;
         return numeros;
     }
+    public static boolean compararNumeros(int num, int array[][]){
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == num){
+                return true;
+            }   
+            }
+        }
+        return false;
+    }
     
     public static void main(String[] args) {
         
         int matriz[][] = new int[3][6];
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = generarNumeros();
+        int numero;
+        try {
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    do {
+                        numero = generarNumeros();
+                        System.out.println(numero);
+                    } while (compararNumeros(numero, matriz));
+                    matriz[i][j] = numero;
+                    
+                }
+            }
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.println(Arrays.toString(matriz[i]));   
             }
             
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
