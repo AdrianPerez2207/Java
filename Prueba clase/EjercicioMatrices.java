08123a5feecf76c0916d7c0d2842312a5c2b54de
AdrianPerez2207 <aperezsanchez441@gmail.com>
2023-12-02 20:40:56 +0100

    casa commit

--- Prueba clase/EjercicioMatrices.java
+++ Prueba clase/EjercicioMatrices.java
@@ -1,6 +1,31 @@
 import java.util.Arrays;
 public class EjercicioMatrices {
 
+    public static int pintarMovimientosCaballo(int i, int j, String tablero[][]){
+        int coordX;
+        int coordY;
+        
+    }
+
+    public static String[] coordenadasAjedrez(int i, int j) {
+        int coordX;
+        char coordY;
+
+        //Array resultado
+        String[] coordenadaResultado = new String[2];
+
+        //Comprobar que la i y la j estén entre 0 y 7
+        if ( (i>=0 && i<=7) && (j>=0 && j<=7) ) {
+            coordX = (8 - i);
+            coordY = (char) (97 + j); //Cogemos un "char" para pasa de un número ASCI a una letra
+            coordenadaResultado[0] = Integer.toString(coordX);
+            coordenadaResultado[1] = Character.toString(coordY);
+        } else {
+            System.out.println("Coordenadas inválidas");
+        }
+
+        return coordenadaResultado;
+    }
     public static void main(String[] args) {
         
         String tablero[][] = new String[8][8];
@@ -73,6 +98,8 @@ public class EjercicioMatrices {
         for (int i = 0; i < tablero.length; i++) {
             System.out.println(Arrays.toString(tablero[i]));
         }
+        coordenadasAjedrez(5, 5);
+        System.out.println(Arrays.toString(coordenadasAjedrez(5, 5)));
     }
     
 }
