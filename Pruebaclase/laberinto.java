ab1ab310f880633a4a57f01556aa9a6316f33404
AdrianPerez2207 <aperezsanchez441@gmail.com>
2023-12-05 14:02:27 +0100

    laberinto commit

---
+++ Pruebaclase/laberinto.java
@@ -0,0 +1,137 @@
+package Pruebaclase;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class laberinto {
+    //Función para pintar la matriz
+    public static void pintarMatriz(String matriz[][]) {
+        for(int i=0; i<matriz.length; i++) {
+            System.out.println(Arrays.toString(matriz[i]));
+        }
+    }
+    //Función para rellenar el Laberinto
+    public static void rellenarLaberinto(String laberinto[][], int salida[], int posicion[]){
+
+        for (int i = 0; i < laberinto.length; i++) {
+            for (int j = 0; j < laberinto[i].length; j++) {
+
+                laberinto[i][j] = ".";
+
+                if ((i == 0 && j != 0) || (i == 19)){
+                    laberinto[i][j] = "-";
+                }
+                if ((j == 0) || (j == 19)){
+                    laberinto[i][j] = "|";
+                }
+                if (i != 0 && j != 0) {
+                }
+                if (salida[0] == i && salida[1] == j){
+                    laberinto[i][j] = "#";
+                }
+                if (posicion[0] == i && posicion[1] == j) {
+                    laberinto[i][j] = "@";
+                } 
+            }
+        }
+    }
+    //Función para generar menú
+    public static int menu(){
+
+        System.out.println("Elige la opción para mover al jugador");
+        System.out.println("1: Derecha");
+        System.out.println("2: Izquierda");
+        System.out.println("3: Abajo");
+        System.out.println("4: Arriba");
+
+        int opcion = Integer.parseInt(System.console().readLine());
+
+        return opcion;
+    }
+
+    //Función para generar números aleatorios
+    public static int generarNumero(int minimo, int maximo){
+        return (int) ((Math.random()*(maximo - minimo + 1) + minimo));
+    }
+
+    //Función para generar una salida aleatoriamente
+    public static int[] generarSalida(){
+        int salida[] = new int[2];
+        salida[0] = generarNumero(0, 19);
+        if (salida[0] == 0){
+            salida[1] = generarNumero(1, 19);
+        } else if (salida[0] == 19){
+            salida[1] = generarNumero(0, 19);
+        } else {
+            int numAleatorio = generarNumero(1, 2);
+            if (numAleatorio == 1){
+                salida[1] = 0;
+            } else {
+                salida[1] = 19;
+            }
+        }
+        return salida;
+    }
+
+    //Función para generar la posición de la bomba
+    public static int[] generarBomba(int[] salida){
+        int bomba[] = new int[2];
+        bomba[0] = generarNumero(1, 18);
+        bomba[1] = generarNumero(1, 18);
+
+        return bomba;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String laberinto[][] = new String[20][20];
+
+        //Creamos un nuevo array y llamamos a la función para generar la salida
+        int salida[] = generarSalida();
+
+        //Array para generar la posición de la bomba
+        int bomba[] = generarBomba(salida);
+
+        //Salida del jugador
+        int posicion[] = {0, 0};
+
+        rellenarLaberinto(laberinto, salida, posicion);
+
+        pintarMatriz(laberinto);
+
+        do {
+            switch (menu()) {
+            case 1:
+                posicion[1]++;                   
+                break;
+            case 2:
+                posicion[1]--;                  
+                break;
+            case 3:
+                posicion[0]++;
+                break;
+            case 4:
+                posicion[0]--;
+                break; 
+            default:
+                break;
+            }
+            if (posicion == salida) {
+            System.out.println("Enhorabuena!! Has encontrado la salida");
+            }
+            if (posicion == bomba) {
+                System.out.println("BOOOM!! Has encontrtado la bomba");
+            }
+            if ((posicion[0] < 0 && posicion[0] > 19) || (posicion[1] < 0 && posicion[1] > 19)){
+                System.out.println("Error!!! Te has salido del tablero");
+            }
+            rellenarLaberinto(laberinto, salida, posicion);
+            pintarMatriz(laberinto);
+
+            
+        } while ((posicion != bomba) || (posicion != salida));
+
+
+    }
+    
+}
