216bf77f43617dced31c73418cafaf4c7fb08264
AdrianPerez2207 <aperezsanchez441@gmail.com>
2023-12-12 13:53:32 +0100

    ejercicio3 commit

--- primeraEvaluacion/ejercicio3.java
+++ primeraEvaluacion/ejercicio3.java
@@ -2,19 +2,40 @@ package primeraEvaluacion;
 import java.util.Arrays;
 public class ejercicio3 {
 
-    public static String convertirAMorse(char morseConvertido){
+    public static String obtenerCodigoMorse(char caracter){
         String morse[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         char letras[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         for (int i = 0; i < letras.length; i++) {
-            if (Character.toUpperCase(morseConvertido) == letras[i]) {
+            if (Character.toUpperCase(caracter) == letras[i]) {
                 return morse[i];
             }
         }
 
         return "";
     }
+    public static String convertirMorse(String texto){
+        StringBuilder morse = new StringBuilder();
+        for (int i = 0; i < texto.length(); i++) {
+            char character = texto.charAt(i);
+
+            if (character == ' ') {
+                morse.append("# ");
+                continue;
+            }
+
+            String morseCode = obtenerCodigoMorse(character);
+            if (!morseCode.equals("")) {
+                morse.append(morseCode).append(" ");
+            }
+        }
+
+
+
+        return morse.toString().trim();
+    }
     public static void main(String[] args) {
-        String texto = "hola mnundo";
+        String texto = "hola mundo";
+        System.out.println(convertirMorse(texto));
 
         
     }
